9069f55835ebd897fe92bd97588740b3a6de01d5
backcost <gabriel_costa_ferreira@hotmail.com>
2023-06-19 16:54:14 -0300

    criando base para galeria

---
+++ src/componentes/Galeria/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Galeria = () => {
+  return (
+    <section>
+        <h2>Navegue pela galeria</h2>
+        <ul></ul>
+    </section>
+  )
+}
+
+export default Galeria
