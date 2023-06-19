81e776cf3243fe000a2b124a69827c155cd5d1dc
backcost <gabriel_costa_ferreira@hotmail.com>
2023-06-19 13:03:36 -0300

    criando menu

---
+++ src/componentes/Menu/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <nav>
+        <ul>
+            <li>
+                <img src='' alt='' />
+                <a href='/'>Inicio</a>
+            </li>
+            <li>
+                <img src='' alt='' />
+                <a href='/'>Mais curtidas</a>
+            </li>
+            <li>
+                <img src='' alt='' />
+                <a href='/'>Mais vistas</a>
+            </li>
+            <li>
+                <img src='' alt='' />
+                <a href='/'>Novas</a>
+            </li>
+            <li>
+                <img src='' alt='' />
+                <a href='/'>Inicio</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Menu
