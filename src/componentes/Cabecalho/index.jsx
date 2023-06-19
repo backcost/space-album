e0003509dfd1d43c4eec5b06c751c34e64d7e14d
backcost <gabriel_costa_ferreira@hotmail.com>
2023-06-19 12:41:24 -0300

    base cabeçalho

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,8 +1,15 @@
-import logo
+import logo from './logo.png'
+import search from './search.png'
 
 const Cabecalho = () => {
   return (
-    <div>Cabecalho</div>
+    <header>
+        <img src={logo} alt="Space Album" />
+        <div>
+            <input type='text' placeholder='O que você procura?' />
+            <img src={search} alt='Busca' />
+        </div>
+    </header>
   )
 }
 
