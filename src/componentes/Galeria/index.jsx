720ca97d6e3a2fa27fb90c89b233c77b71bd2019
backcost <gabriel_costa_ferreira@hotmail.com>
2023-06-19 18:21:36 -0300

    CARDS

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Tags from '../Tags'
+import styles from './Galeria.module.scss'
+import Cards from './Cards'
+import fotos from './fotos.json'
 
 const Galeria = () => {
   return (
-    <section>
+    <section className={styles.galeria}>
         <h2>Navegue pela galeria</h2>
         <Tags />
-        <ul></ul>
+        <Cards itens={fotos} style={styles}/>
     </section>
   )
 }
--- src/componentes/Tags/index.jsx
+++ src/componentes/Tags/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './Tags.module.scss'
 
 const Tags = () => {
   return (
