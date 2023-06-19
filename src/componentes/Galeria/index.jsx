facd65218b953d525ea999791b6f92563e555e80
backcost <gabriel_costa_ferreira@hotmail.com>
2023-06-19 16:57:41 -0300

    montando tags

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Tags from '../Tags'
 
 const Galeria = () => {
   return (
     <section>
         <h2>Navegue pela galeria</h2>
+        <Tags />
         <ul></ul>
     </section>
   )
---
+++ src/componentes/Tags/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Tags = () => {
+  return (
+    <div>
+        <p>Filtre por tagas:</p>
+        <ul>
+            <li>Estrelas</li>
+            <li>Galáxias</li>
+            <li>Lua</li>
+            <li>Planetas</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Tags
